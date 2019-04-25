ebc7c4329f4a565b4e8b7980a353b4f5437aa82d
Marc Davies <marcdaviesriot@gmail.com>
2019-04-25 01:03:07 -0700

    save lesson example to GitHub

---
+++ Shapes/Models/Cube.cs
@@ -0,0 +1,26 @@
+using System;
+using System.Collections.Generic;
+
+namespace Shapes.Models
+{
+  public class Cube
+  {
+    private Rectangle _face;
+    public Cube (Rectangle side)
+    {
+      _face = side;
+    }
+
+    public int GetVolume()
+    {
+      int length = _face.GetLength();
+      return length * length * length;
+    }
+
+    public int GetSurfaceArea()
+    {
+      int length = _face.GetLength();
+      return (length * length) * 6;
+    }
+  }
+}
